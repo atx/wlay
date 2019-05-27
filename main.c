7b95d64e1171ac91b6745f2ad786ffbd66651638
Josef Gajdusek <atx@atx.name>
2019-05-27 22:08:33 +0200

    Fix 3-tab offset
    Was making my vim-sleuth crazy.

--- main.c
+++ main.c
@@ -38,13 +38,13 @@
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
 #define max(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-      __typeof__ (b) _b = (b); \
-      _a > _b ? _a : _b; })
+    ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+       _a > _b ? _a : _b; })
 #define min(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-      __typeof__ (b) _b = (b); \
-      _a < _b ? _a : _b; })
+    ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+       _a < _b ? _a : _b; })
 
 enum wlay_config_type {
     WLAY_CONFIG_SWAY,
